7437dc53f3a2a22d6137dfc5bcd419afe80dc03f
Mansi Pandey <125549121+MansiPandey-2002@users.noreply.github.com>
2023-09-06 15:08:33 +0000

    Create index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+// Wait for the DOM to be fully loaded
+document.addEventListener("DOMContentLoaded", function () {
+    // Animation for the header text
+    // Animation for the header text
+    anime.timeline({
+        targets: ".name, .opacity-75, .btn-outline-light",
+        easing: "easeInOutExpo",
+    })
+    .add({
+        translateY: [50, 0],
+        opacity: [0, 1],
+        duration: 1000,
+        delay: (el, i) => 300 * i,
+    })
+    // Chain the works section animation after the header animation
+    .add({
+        targets: ".col-lg-4",
+        translateX: [-50, 0],
+        opacity: [0, 1],
+        duration: 1000,
+        delay: function(el, i) {
+            return i * 150; // Adjust the delay to control the gap between columns
+        },
+    });
+});
+
