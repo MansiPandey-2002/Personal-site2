69507712a853361c23dade04680c3964bbb78d0e
Mansi Pandey <125549121+MansiPandey-2002@users.noreply.github.com>
2023-09-07 16:19:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -24,3 +24,12 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
+document.addEventListener("DOMContentLoaded", function () {
+    const menuToggle = document.getElementById("menu-toggle");
+    const navList = document.getElementById("nav-list");
+
+    menuToggle.addEventListener("click", function () {
+        navList.classList.toggle("active");
+    });
+});
+
